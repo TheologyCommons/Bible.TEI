d0cce7b340c02b5f7c61ab9132b766f2328b017e
David.Strickland <Davidstrickland0@hotmail.com>
2018-12-11 23:12:59 +0000

    Formatted xml and json for KJV completed in TEI format

--- AlterText/Program.cs
+++ AlterText/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Xml;
+using Newtonsoft.Json;
 
 namespace AlterText
 {
@@ -10,50 +11,12 @@ namespace AlterText
     {
         static void Main(string[] args)
         {
-            
+            // To convert an XML node contained in string xml into a JSON string   
+            XmlDocument doc = new XmlDocument();
+            doc.Load("../../../../KJV.xml");
+            string jsonText = JsonConvert.SerializeXmlNode(doc.DocumentElement);
 
-            string text = File.ReadAllText("../../../../KJV.xml", Encoding.UTF8);
-
-            text = Regex.Replace(text, $"<p>[\\d]+:[\\d]+\\s", "<p>");
-
-            string result = "";
-
-            MemoryStream mStream = new MemoryStream();
-            XmlTextWriter writer = new XmlTextWriter(mStream, Encoding.Unicode);
-            XmlDocument document = new XmlDocument();
-
-            try
-            {
-                // Load the XmlDocument with the XML.
-                document.LoadXml(text);
-
-                writer.Formatting = Formatting.Indented;
-
-                // Write the XML into a formatting XmlTextWriter
-                document.WriteContentTo(writer);
-                writer.Flush();
-                mStream.Flush();
-
-                // Have to rewind the MemoryStream in order to read
-                // its contents.
-                mStream.Position = 0;
-
-                // Read MemoryStream contents into a StreamReader.
-                StreamReader sReader = new StreamReader(mStream);
-
-                // Extract the text from the StreamReader.
-                string formattedXml = sReader.ReadToEnd();
-
-                result = formattedXml;
-            }
-            catch (XmlException)
-            {
-                // Handle the exception
-            }
-
-
-
-            File.WriteAllText("../../../../KJV1.xml", result);
+            File.WriteAllText("../../../../KJV.json", jsonText);
         }
     }
 }
